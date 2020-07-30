b411f37cc159f9292ced4f5c47268bf3dfad46ec
bunnyqian <54897566+bunnyqian@users.noreply.github.com>
2020-07-30 15:01:27 +0800

    Update HomeControllerTest.cs

--- AspNetCoreWebApplicationTest/Controllers/HomeControllerTest.cs
+++ AspNetCoreWebApplicationTest/Controllers/HomeControllerTest.cs
@@ -15,13 +15,13 @@ namespace AspNetCoreWebApplicationTest.Controllers
             Assert.Equal("You just created a ASP.Net Core web application!", result.ViewData["Message"]);
         }
         
-        [Fact]
+        /*[Fact]
         public void NoInputParamGetResponseTest()
         {
             HelloController controller = new HelloController();
             var response = controller.Get().Value as Response;
             Assert.Equal("Hello World!!!", response.output);
-        }
+        }*/
 
         [Fact]
         public void ErrorTest()
